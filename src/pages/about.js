11e642d1cc55f1048d50cb831f680f3499892d4f
Quyen Tran <qtrandev@gmail.com>
2019-09-10 19:55:46 +0700

    Add About page home button

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class about extends Component {
   render() {
     return (
-      <div>
+      <div className="about-pane">
         <h2>About Page</h2>
 
         <p>This is the fruit app.</p>
+
+        <Link to="/"><button>Home</button></Link>
       </div>
     )
   }
